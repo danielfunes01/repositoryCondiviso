81f3500a35258dc424419cb4ca2a132ed35eb162
admin <admin@DESKTOP-R9FU8US.station>
2022-09-11 09:25:41 +0200

    rilascio ut2

--- AppDiTest/src/it/dan/test/Prova1.java
+++ AppDiTest/src/it/dan/test/Prova1.java
@@ -6,24 +6,7 @@ public class Prova1 {
 		
 		System.out.println("BENVENUTO");
 		
-//DSFDF
-		// MODIFICA UTENTE 2
-		
-		// modifica utente uno ciao pippo
-		
-        System.out.println("INIZIO PTIC NUOVA");
-		
-		int a=9;
-		int b=0;
-        int c=6;
-        int mod=10;
 
-
-		
-		int e=8;
-		String s="";
-
-		int r=33;
 		
 	}
 
